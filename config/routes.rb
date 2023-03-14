f239ab0229b26870618827ff973ca16b51835cbc
mamime19 <tatsu25sun@gmail.com>
2023-03-14 12:46:30 +0900

    migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-end
+      devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:index, :show]
-  root 'users#index'
 end
--- db/migrate/20230313103204_create_lovetrees.rb
+++ db/migrate/20230313103204_create_lovetrees.rb
@@ -2,7 +2,7 @@ class CreateLovetrees < ActiveRecord::Migration[7.0]
   def change
     create_table :lovetrees do |t|
       t.integer :growth
-
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
