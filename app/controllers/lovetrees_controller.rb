025c1014a6465e7be1aff2eb898f754613e7ddc5
mamime19 <tatsu25sun@gmail.com>
2023-03-14 16:05:38 +0900

    tree

--- app/controllers/lovetrees_controller.rb
+++ app/controllers/lovetrees_controller.rb
@@ -1,3 +1,5 @@
 class LovetreesController < ApplicationController
-	
+	def show
+		@lovetree=Lovetree.find(current_user.id)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
-      devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
-    end
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
+  resources :users do
+    resource :lovetree
+  end
+  get 'user/:id/lovetree' ,to: 'lovetree#show'
   root "users#index"
 end
